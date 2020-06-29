9285ffc845421e45dc89926836f4ab751fb66efc
abhishree1 <abhishreekhangar01@gmail.com>
2020-06-30 00:36:24 +0530

    some stuff added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-   mailto="mailto: abhishreekhangar01@gmail.com";
-    name="Abhishree Khangar.";
+  mailto="mailto: abhishreekhangar01@gmail.com";
+  name="Abhishree Khangar.";
+  about=" Hello! I'm Abhishree, a software engineer. I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. My goal is to always build products that provide pixel-perfect, performant experiences.";
+  about2="Here are a few technologies I've been working with recently:";
+  tech=["HTML & (S)CSS","Javascript","Angular 9","Java","Python"];
   constructor() { }
 
   ngOnInit(): void {
